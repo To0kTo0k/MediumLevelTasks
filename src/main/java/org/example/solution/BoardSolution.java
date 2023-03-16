e4625e4f1a8fb0f310eece04cc9ec0f8f9bf3180
gorodovss22pv191 <sergejgorodov096@gmail.com>
2023-03-16 08:02:04 -0400

    fix number 3

--- src/main/java/org/example/solution/BoardSolution.java
+++ src/main/java/org/example/solution/BoardSolution.java
@@ -1,7 +1,12 @@
 package org.example.solution;
 
+import java.util.Arrays;
+import java.util.HashSet;
+import java.util.Set;
+
 public class BoardSolution {
-    private static final char[] symbols = {'1', '2', '3', '4', '5', '6', '7', '8', '9', '.'};
+    private static final Character[] SYMBOLS = {'1', '2', '3', '4', '5', '6', '7', '8', '9', '.'};
+    public static final Set<Character> SET_SYMBOLS = new HashSet<>(Arrays.asList(SYMBOLS));
 
     /**
      * Матрица должна быть валидна в соответствии с правилами:
@@ -11,32 +16,31 @@ public class BoardSolution {
      * 4) Значение ячейки валидно, если это цифра от 1 до 9 или .
      * 5) Наличие букв в качестве значений недопустимо
      **/
-    public static boolean isValidBoard(char[][] board) {
-        return isValidSymbols(board) && isValidRows(board) && isValidColumns(board);
+    public static boolean isValidBoard() {
+        char[][] board =
+                {{'5', '3', '.', 'a', '7', '.', '.', '.', '.'}
+                        , {'6', '.', '.', '1', '9', '5', '.', '.', '.'}
+                        , {'.', '9', '8', '.', '.', '.', '.', '6', '.'}
+                        , {'8', '.', '.', '.', '6', '.', '.', '.', '3'}
+                        , {'4', '.', '.', '8', '.', '3', '.', '.', '1'}
+                        , {'7', '.', '.', '.', '2', '.', '.', '.', '6'}
+                        , {'.', '6', '.', '.', '.', '.', '2', '8', '.'}
+                        , {'.', '.', '.', '4', '1', '9', '.', '.', '5'}
+                        , {'.', '.', '.', '.', '8', '.', '.', '7', '9'}};
+        return isValidSymbols(board) && isValidRowsAndColumns(board);
     }
 
     /**
-     * Проверка строк на уникальные цифровые значения
+     * Проверка строк и столбцов на уникальные цифровые значения
      **/
-    private static boolean isValidRows(char[][] board) {
-        for (char[] chars : board) {
-            for (int j = 0; j < board.length - 1; j++) {
-                if (chars[j] == chars[j + 1] && chars[j] != '.') {
-                    return false;
-                }
-            }
-        }
-        return true;
-    }
-
-    /**
-     * Проверка столбцов на уникальные значения
-     **/
-    private static boolean isValidColumns(char[][] board) {
-        for (int i = 0; i < board.length; i++) {
-            for (int j = 0; j < board.length - 1; j++) {
-                if (board[j][i] == board[j + 1][i] && board[j][i] != '.') {
-                    return false;
+    private static boolean isValidRowsAndColumns(char[][] board) {
+        for (int k = 0; k < board.length; k++) {
+            for (int i = 0; i < board.length - 1; i++) {
+                for (int j = i + 1; j < board.length; j++) {
+                    if ((board[k][i] == board[k][j] && board[k][i] != '.') || (board[i][k] == board[j][k] && board[i][k] != '.')) {
+                        System.out.println("There are similar symbols in [" + k + ", " + i + "] and [" + k + ", " + j + "] or/and in [" + i + ", " + k + "] and [" + j + ", " + k + "]");
+                        return false;
+                    }
                 }
             }
         }
@@ -47,15 +51,10 @@ public class BoardSolution {
      * Проверка на валидность символов
      **/
     private static boolean isValidSymbols(char[][] board) {
-        for (char[] chars : board) {
+        for (int i = 0; i < board.length; i++) {
             for (int j = 0; j < board.length; j++) {
-                boolean flag = false;
-                for (char symbol : symbols) {
-                    if (symbol == chars[j]) {
-                        flag = true;
-                    }
-                }
-                if (!flag) {
+                if (!SET_SYMBOLS.contains(board[i][j])) {
+                    System.out.println("Invalid symbol in position [" + i + ", " + j + "]: " + board[i][j]);
                     return false;
                 }
             }
