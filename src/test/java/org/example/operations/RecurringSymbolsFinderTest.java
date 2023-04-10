a0cd418df5ae7cf84590a4363035778da9197c49
gorodovss22pv191 <sergejgorodov096@gmail.com>
2023-04-10 08:23:07 -0400

    Add more tests

--- src/test/java/org/example/operations/RecurringSymbolsFinderTest.java
+++ src/test/java/org/example/operations/RecurringSymbolsFinderTest.java
@@ -10,10 +10,32 @@ import static org.junit.jupiter.api.Assertions.*;
 class RecurringSymbolsFinderTest {
 
     @Test
-    void findRecurringSymbolSymbolsTest() {
+    void findNoSymbolsTest() {
         RecurringSymbolsFinder finder = new RecurringSymbolsFinder();
         Map<Character, Integer> recurringSymbols = finder.findRecurringSymbols("");
         Map<Character, Integer> expectedSymbols = new HashMap<>();
         assertEquals(expectedSymbols, recurringSymbols);
+    }
+
+    @Test
+    void findRecurringSymbolsTest() {
+        RecurringSymbolsFinder finder = new RecurringSymbolsFinder();
+        Map<Character, Integer> recurringSymbols = finder.findRecurringSymbols("aa ab");
+        Map<Character, Integer> expectedSymbols = new HashMap<>();
+        expectedSymbols.put('a', 3);
+        expectedSymbols.put('b', 1);
+        expectedSymbols.put(' ', 1);
+        assertEquals(expectedSymbols, recurringSymbols);
+    }
+
+    @Test
+    void findDifferentCaseSymbolsTest() {
+        RecurringSymbolsFinder finder = new RecurringSymbolsFinder();
+        Map<Character, Integer> recurringSymbols = finder.findRecurringSymbols("Aa");
+        int expectedSymbolsNumber = 2;
+        int realSmallSymbolsNumber = recurringSymbols.get('a');
+        int realBigSymbolsNumber = recurringSymbols.get('A');
+        assertEquals(expectedSymbolsNumber, realSmallSymbolsNumber);
+        assertEquals(expectedSymbolsNumber, realBigSymbolsNumber);
     }
 }
