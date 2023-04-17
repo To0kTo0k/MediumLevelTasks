7debed0592cdb2e0b52cd00ad004b09339e4e9d2
gorodovss22pv191 <sergejgorodov096@gmail.com>
2023-04-17 08:13:54 -0400

    Full Streamless FirstUnrecurringSymbolFinder class realisation

--- src/main/java/org/example/handler/FirstUnrecurringSymbolFinder.java
+++ src/main/java/org/example/handler/FirstUnrecurringSymbolFinder.java
@@ -1,20 +1,19 @@
 package org.example.handler;
 
-import java.util.Arrays;
-
 public class FirstUnrecurringSymbolFinder {
     public Character findFirstUnrecurringSymbol(String symbolLine) {
         if (symbolLine == null || symbolLine.isEmpty()) {
             return Character.MIN_VALUE;
         }
         int[] array = new int[123];
-        Arrays.fill(array, 0);
         for (char symbol : symbolLine.toCharArray()) {
             array[symbol]++;
         }
-        return symbolLine.chars()
-                .mapToObj(s -> (char) s)
-                .filter(symbol -> array[symbol] == 1)
-                .findFirst().orElse(Character.MIN_VALUE);
+        for (char symbol : symbolLine.toCharArray()) {
+            if (array[symbol] == 1) {
+                return symbol;
+            }
+        }
+        return Character.MIN_VALUE;
     }
 }
