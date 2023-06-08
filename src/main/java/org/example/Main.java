f803c8eda965653f5915c7f471cd447047fc513f
gorodovss22pv191 <sergejgorodov096@gmail.com>
2023-06-08 06:57:02 -0400

    One more try to delete .idea from remote branch.

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -2,9 +2,33 @@ package org.example;
 
 import org.example.handler.FirstUnrecurringSymbolFinder;
 
+import java.util.Random;
+
 public class Main {
+
     public static void main(String[] args) {
+        String symbolLine = randomString();
+
+        System.out.println(symbolLine);
         FirstUnrecurringSymbolFinder finder = new FirstUnrecurringSymbolFinder();
-        System.out.println("\"" + finder.findFirstUnrecurringSymbol("ab abc") + "\"");
+        long start = System.currentTimeMillis();
+        char symbol = finder.findFirstUnrecurringSymbol(symbolLine);
+        long finish = System.currentTimeMillis();
+        System.out.println("\"" + symbol + "\" за время в " + (finish - start) + " миллисекунд");
+    }
+
+    public static String randomString() {
+        int leftLimit = 66; // numeral 'B'
+        int rightLimit = 122; // letter 'z'
+        int targetStringLength = 999999;
+        Random random = new Random();
+
+        String generatedString = random.ints(leftLimit, rightLimit + 1)
+                .filter(i -> (i >= 66) && (i <= 90 || i >= 97))
+                .limit(targetStringLength)
+                .collect(StringBuilder::new, StringBuilder::appendCodePoint, StringBuilder::append)
+                .toString();
+        generatedString = generatedString + 'A';
+        return generatedString;
     }
 }
