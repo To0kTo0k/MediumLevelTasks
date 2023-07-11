3864d8834bc28b3cba2d247eff78083a89be95c3
gorodovss22pv191 <sergejgorodov096@gmail.com>
2023-07-11 03:02:09 -0400

    Add demo to Main.

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,7 +1,23 @@
 package org.example;
 
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        TreeOperations operations = new TreeOperations();
+
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Input number of tree elements:");
+        int number = scanner.nextInt();
+        System.out.println("Input array of int values:");
+        int [] values = new int[number];
+        for (int i = 0; i < number; i++) {
+            values[i] = scanner.nextInt();
+        }
+        Node tree = operations.init(values);
+        System.out.println("Preorder tree traversal:\n" + operations.searchNLR(tree) + "\n");
+        System.out.println("Inorder tree traversal:\n" + operations.searchLNR(tree) + "\n");
+        System.out.println("Postorder tree traversal:\n" + operations.searchLRN(tree) + "\n");
+        System.out.println("Breadth-first traversal:\n" + operations.searchBFS(tree) + "\n");
     }
 }
